c47dd3a6cadd1ab4dadb27eb7bfb339b29c28a25
Joshua Gray <j.d.gray@outlook.com>
2019-04-11 18:06:53 -0700

    Adding homework for week 1

---
+++ Homework1/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Windows;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Models.User user = new Models.User();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            uxSubmit.IsEnabled = false;
+            //WindowState = WindowState.Maximized;
+        }
+
+        public override void EndInit()
+        {
+            base.EndInit();
+            uxContainer.DataContext = user;
+        }
+
+        private void UxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password:" + uxPassword.Text);
+        }
+
+        private void nameTextChange(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if(IsNameOrPasswordError())
+            {
+                uxSubmit.IsEnabled = true;
+            }
+            else
+            {
+                uxSubmit.IsEnabled = false;
+            }
+        }
+
+        private void passwordTextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (IsNameOrPasswordError())
+            {
+                uxSubmit.IsEnabled = true;
+            }
+            else
+            {
+                uxSubmit.IsEnabled = false;
+            }
+
+        }
+
+        private bool IsNameOrPasswordError()
+        {
+            return ((user.NameError == null || user.NameError == "") && (user.PasswordError == null || user.PasswordError == ""));
+        }
+    }
+}
---
+++ Homework1/HelloWorld/Models/User.cs
@@ -0,0 +1,137 @@
+﻿using System.ComponentModel;
+namespace HelloWorld.Models
+{
+    class User : IDataErrorInfo, INotifyPropertyChanged
+    {
+        private string name = string.Empty;
+        private string password = string.Empty;
+        private string nameError;
+        private string passwordError;
+
+        public string NameError
+        {
+            get
+            {
+                return nameError;
+            }
+            set
+            {
+                if (nameError != value)
+                {
+                    nameError = value;
+                    OnPropertyChanged("NameError");
+                }
+            }
+        }
+
+        public string PasswordError
+        {
+            get
+            {
+                return passwordError;
+            }
+            set
+            {
+                if(passwordError != value)
+                {
+                    passwordError = value;
+                    OnPropertyChanged("PasswordError");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+
+        // IDataErrorInfo interface
+        public string Error
+        {
+            get
+            {
+                return NameError;
+            }
+        }
+
+        // IDataErrorInfo interface
+        // Called when ValidatesOnDataErrors=True
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case "Name":
+                        {
+                            NameError = "";
+                            if (string.IsNullOrEmpty(Name) || Name.Length == 0)
+                            {
+                                NameError = "Name cannot be empty.";
+                            }
+                            if (Name.Length > 12)
+                            {
+                                NameError = "Name cannot be longer than 12 characters.";
+                            }
+                            return NameError;
+                        }
+                    case "Password":
+                        {
+                            PasswordError = "";
+                            if(string.IsNullOrEmpty(Password) || Password.Length == 0)
+                            {
+                                PasswordError = "Password cannot be empty.";
+                            }
+                            if(Password.Length < 10)
+                            {
+                                PasswordError = "Password must be longer than 9 characters.";
+                            }                           
+                            return PasswordError;
+                        }
+                    default:
+                        {
+                            return null;
+                        }
+                }
+            }
+        }
+
+        // INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
