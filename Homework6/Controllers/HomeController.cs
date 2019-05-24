3f95d8c9811d5caf731086131acaed73b2ccc1fe
Joshua Gray <j.d.gray@outlook.com>
2019-05-23 18:34:27 -0700

    adding Homework 6

---
+++ Homework6/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Homework6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework6.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("CardSent", card);
+            }
+            else return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ Homework6/Models/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Homework6.Models
+{
+    public class Card
+    {
+        [Required(ErrorMessage="Please enter a Recipient")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please enter a Sender")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Please enter a Message")]
+        public string Message { get; set; }
+    }
+}
